afe4773d92fe63a2f6ed205c31c87ef880312159
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-05-15 19:13:06 -0500

    Calcalytics
    Calcalytics

--- scripts.js
+++ scripts.js
@@ -2,6 +2,6 @@ $(document).ready(pageReady);
 
 
 function pageReady(){
-    alert("jQuery init");
-    console.log("We are in jQuery");
+    // alert("jQuery init");
+    // console.log("We are in jQuery");
 } 
