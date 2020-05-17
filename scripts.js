d3fd4919bf29fdd363422a49cb64683915d9d0aa
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-05-17 05:00:09 -0500

    Adjustments

--- scripts.js
+++ scripts.js
@@ -32,18 +32,28 @@ function inputCollect() {
     let iDEntry = $('#iD').val();
     let jobTitleEntry = $('#jobTitle').val();
     let annualSalaryEntry = $('#annualSalary').val();
-    let sub
-    if ($(".employeeInputFields").val() == '') { //the logic here is correct but we aren't linking the data fields to anything so it's always going to Bork!
-        alert('Bork Bork Bork');
-    } else {
-        alert('Your data is up to date an accurate');
-    }
-//I GOT IT TO WORK!  Nice. :].
 
+    $('.apenderOoo').children('tbody').append(`<tr>                            
+<td>${firstNameEntry}</td>   
+<td>${lastNameEntry}</td>
+<td>${iDEntry}</td>
+<td>${jobTitleEntry}</td>
+<td>${annualSalaryEntry}</td>
+<td><button class = "delete" >Delete</button></td>    
+</tr>`);
+
+
+
+
+
+    // if ($(".employeeInputFields").val() == '') { //the logic here is correct but we aren't linking the data fields to anything so it's always going to Bork!
+    //     alert('Bork Bork Bork');
+    // } else {
+    //     alert('Your data is up to date an accurate');
+    // }
+
+    //I GOT IT TO WORK!  Nice. :].
     //   console.log("You must fill in all fields before continuing. SpLaT"); //trying to see if we can make a function to return splat if 
     //any of our input fields are empty.
+
 }
-
-// function appenderoo() {
-
-// }
