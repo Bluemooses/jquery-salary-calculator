455e4da227b338f5bbf5ee60f5c0e4114e3bc3bf
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-05-17 07:04:57 -0500

    MY BUTTONS FUNCTION!
    Now I need to create a case where it borks if there is an EMPTY field.

--- scripts.js
+++ scripts.js
@@ -4,10 +4,11 @@ let employeesEntered = [];
 
 function pageReady() {
     $('.masterSubmit').on('click', inputCollect); //If we click masterSubmit class, run inputCollect function.  This is getting intuitive!  
+    $('frameWork').on('click', appendEmployee);
 }
 
 function inputCollect() {
-    let employeeUnit = {
+    let employeeUnit = { //An employee object.
         firstNameEntry: $('#firstName').val(), //these variables will allow us to acces our input.
         lastNameEntry: $('#lastName').val(),
         iDEntry: $('#iD').val(),
@@ -15,26 +16,33 @@ function inputCollect() {
         annualSalaryEntry: $('#annualSalary').val()
     }
     employeesEntered.push(employeeUnit); //we are pushing the employee on click as an object to a new array of objects.
-    console.log("Thanks for logging your employee. Here is employeesEntered:", employeesEntered);//Nice.  This works and is super exciting.
+    console.log("Thanks for logging your employee. Here is employeesEntered:", employeesEntered); //Nice.  This works and is super exciting.
+    $('this').toggleClass('inputCollector');
 }
 
 
-//     $('.apenderOoo').children('tbody').append(`<tr>                            
-// <td>${firstNameEntry}</td>   
-// <td>${lastNameEntry}</td>
-// <td>${iDEntry}</td>
-// <td>${jobTitleEntry}</td>
-// <td>${annualSalaryEntry}</td>
-// <td><button class = "delete" >Delete</button></td>    
-// </tr>`);
+
 function appendEmployee(employeeUnit) {
+    console.log("In appendEmployee");
+    if (employeeUnit.val() == ' ') {
+        console.log("Please fill in all information about employee");
+        alert("Bork bork bork")
+    } else {
+        for (let i = 0; i < employeesEntered; i++) {
+            $('.frameWork').append(`
+        <li>${employeeUnit[i].firstNameEntry}</li>
+        <li>${employeeUnit[i].lastNameEntry}</li>
+        <li>${employeeUnit[i].iDEntry}</li>
+        <li>${employeeUnit[i].jobTitleEntry}</li>
+        <li>${employteeUnit[i].annualSalaryEntry}</li>
+        <button class ="deleteBtn">Delete</button>        
+        `)
+        }
+        console.log("inside of appendEmployee");
 
+    }
 }
 
+// function calculateMonthlyCosts(employeeUnit) {
 
-
-
-    //I GOT IT TO WORK!  Nice. :].
-    //   console.log("You must fill in all fields before continuing. SpLaT"); //trying to see if we can make a function to return splat if 
-    //any of our input fields are empty.
-
+// }
