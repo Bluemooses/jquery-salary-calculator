226d1f0cd4ccc2397c7ed5f688bd43ad545e7acc
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-05-16 22:34:57 -0500

    It works!

--- scripts.js
+++ scripts.js
@@ -26,19 +26,19 @@ function calculateMonthlyCost() {
 }
 
 function inputCollect() {
-    // let firstName = $
-    let firstNameEntry = $('#firstName')
-    let lastNameEntry = $('lastName')
-    let iDEntry = $('iDEntry')
-    let jobTitleEntry = $('jobTitleEntry')
-    let annualSalaryEntry = $('annualSalaryEntry')
-    
-    if ($(".employeeInputFields").val() != '') { //the logic here is correct but we aren't linking the data fields to anything so it's always going to Bork!
+
+    let firstNameEntry = $('#firstName').val(); //these variables will allow us to acces our input.
+    let lastNameEntry = $('#lastName').val();
+    let iDEntry = $('#iD').val();
+    let jobTitleEntry = $('#jobTitle').val();
+    let annualSalaryEntry = $('#annualSalary').val();
+    let sub
+    if ($(".employeeInputFields").val() == '') { //the logic here is correct but we aren't linking the data fields to anything so it's always going to Bork!
         alert('Bork Bork Bork');
     } else {
         alert('Your data is up to date an accurate');
     }
-
+//I GOT IT TO WORK!  Nice. :].
 
     //   console.log("You must fill in all fields before continuing. SpLaT"); //trying to see if we can make a function to return splat if 
     //any of our input fields are empty.
