0561833cbd7cb2f5f0bcb8f153eed45cd5415916
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-05-17 16:10:52 -0500

    Debugging

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(pageReady);
 let employeesEntered = [];
-let monthlyCost = 0;
+let annualLabor = 0;
+
 
 function pageReady() {
     $('.masterSubmit').on('click', inputCollect); //If we click masterSubmit class, run inputCollect function.  This is getting intuitive!  
-    $('#iAppend').on('click', '.deleteBtn', deleteField); //tells us that while targeting iAppend on HTML if deleteBtn gets "click"ed, we run function 
+    $('#iAppend').on('click', '.deleteBtn', deleteField); //tells us that while targeting iAppend id if deleteBtn gets "click"ed, we run function 
 };
 
 function inputCollect() {
@@ -15,28 +16,23 @@ function inputCollect() {
         jobTitleEntry: $('#jobTitle').val(),
         annualSalaryEntry: $('#annualSalary').val()
     };
-
-
     employeesEntered.push(employeeUnit); //we are pushing the employee on click as an object to a new array of objects.
     console.log("Thanks for logging your employee. Here is employeesEntered:", employeesEntered); //Nice.  This works and is super exciting.
     // $('this').toggleClass(inputCollector);
-
-    $('#iAppend').empty(); //Nice this is super handy because we basically said look, I have this information on the DOM, if you want to use it in any efficient fashion you need to only look at one instance at a time.  How do we do that?  By emptying the datafields and only accessing the instance the button is clicked and the fields are full:]
-    appendEmployee();
-    // inputCollect(' '); //Making sure our value in inputCollect function which stores all of our cool variables which can let us target fun stuff clears out so we can do it again if we want.
+    // $('#iAppend').empty(); //Nice this is super handy because we basically said look, I have this information on the DOM, if you want to use it in any efficient fashion you need to only look at one instance at a time.  How do we do that?  By emptying the datafields and only accessing the instance the button is clicked and the fields are full:]
+    appendEmployee(); //running our really cool appendEmployee function.
 }
 
 function appendEmployee() {
-    console.log("In appendEmployee");
-
+    // console.log("In appendEmployee");
+    $('#iAppend').empty();
+    annualLabor = 0;
     for (let i = 0; i < employeesEntered.length; i++) {
-        // if ($('this'.val() = ' ')) { //Here we have an array of objects. I want to cycle through.  If any elements are empty we will bork. (hopefully???)
-        //     console.log("Please fill in all information about employee");
-        //     alert("Bork bork bork")
-        // } else {
+        annualLabor += Number(employeesEntered[i].annualSalaryEntry);
+        // console.log('annualLabor', annualLabor); //Functions as intended.
 
         $('#iAppend').append(`
-        <tr>
+        <tr>  
             <td>${employeesEntered[i].firstNameEntry}</td>
             <td>${employeesEntered[i].lastNameEntry}</td>
             <td>${employeesEntered[i].iDEntry}</td>  
@@ -47,24 +43,47 @@ function appendEmployee() {
     
         `);
     }
-    calcMonthlyCost();
-
+    calcMonthlyCost(); //calling our new function calcMonthlyCost which we will use to help us put an inline "counter" on monthlyLabor
 }
 
 function calcMonthlyCost() {
-    let annualLabor = 0;
-    for (let i = 0; i < employeesEntered.length; i++) {
-        annualLabor += Number(employeesEntered[i].annualSalaryEntry);
-        let monthlyLabor = annualLabor / 12;
-        console.log('annualLabor');
-        if (monthlyLabor >= 20000) {
-            $('.calculatorSalary').css("background-color", "red");
-            alert("Caution, monthly cost is out of this world!", monthlyLabor);
+    let monthlyLabor = annualLabor / 12;
+    // console.log(monthlyLabor); //this logs the proper value from our previous function.  We can now use this data in our helper function to achieve our goals.
 
-        };
-        $('.inlineCalculatorSalary').text(monthlyLabor);
-    }
-};
+
+}
+
+
+
+
+
+
+
+
+
+// function calcMonthlyCost() {
+//     let annualLabor = 0;
+//     let monthlyLabor = annualLabor / 12;
+//     $('.inlineCalculatorSalary').text(monthlyLabor);
+//     // console.log('annualLabor'); //functions as intended.
+//     if (monthlyLabor >= 20000) {
+//         $('.calculatorSalary').css("background-color", "red"); //this visual alert works so I am commenting out my alert for debugging.
+//     } // alert("Caution, monthly cost is out of this world!", monthlyLabor);            
+
+
+// calcCostHelper(); artifact of old ideas.
+
+
+
+
+// function calcCostHelper() {
+
+//     console.log("Is this my helper function now?")
+//     let monthlyLabor = annualLabor / 12;
+//     $('.inlineCalculatorSalary').text(monthlyLabor);
+// }
+
+
 //     annualSalaryTotal += Number(employeesEntered[i].annualSalaryEntry << 0); //Adds all of the element annualSalaryEntry at index until for loop iterates through the entire array.
 //     let monthlyCost = annualSalaryTotal / 12;
 //     console.log("Monthly cost of employees on bankroll:", monthlyCost);
@@ -80,6 +99,7 @@ function calcMonthlyCost() {
 
 
 
+///////////////////////////////////////////////That works so stay down there.
 
 
 function deleteField() {
